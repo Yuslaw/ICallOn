8506965f315c86e0cbcd15861cdb0bca5d397867
oladeji mujeeb <oladejimujeeb@yahoo.com>
2022-04-19 16:27:57 +0100

    Game Repository

--- api/Interface/IRepositories/IGameRepository.cs
+++ api/Interface/IRepositories/IGameRepository.cs
@@ -1,20 +1,19 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using api.Entities;
 
 namespace api.Interface.IRepositories
 {
     public interface IGameRepository
     {
-        public Game AddGame(Game game);
-        public Game UpdateGame(Game game);
-        public bool DeleteGame(int id);
-        public Game GetGame(int id);
-        public IList<Game> GetAll();
+        public Task<Game> AddGame(Game game);
+        public Task<Game> UpdateGame(Game game);
+        public bool DeleteGame(Game game);
+        public Task<Game> GetGame(int id);
+        public Task<IList<Game>> GetAll();
+
+        public Task<Game> GetGameByTitle( string title);
         /*public IList<PlayerDto> GetPlayersByGame(int id);*/
-        
-        
-
-
 
 
     }
