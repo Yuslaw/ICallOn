ca84d229a33f445682fd79d6e4c04974b535cf76
AbdussalaamR <sannirukayat@gmail.com>
2022-04-19 15:39:02 +0100

    Implementations

--- api/Interface/IServices/IEntryService.cs
+++ api/Interface/IServices/IEntryService.cs
@@ -11,8 +11,6 @@ namespace api.Interface.IServices
     {
         public Task<BaseResponse> CreateEntry(CreateEntryRequestModel model);
         public Task<BaseResponse> UpdateEntry(UpdateEntryRequestModel model);
-        public Task<IList<EntriesResponseModel>> GetEntriesByExpression(Expression<Func<Entry, bool>> expression);
-        public Task<IList<EntriesResponseModel>> GetEntries();
-        public Task<EntryResponseModel> GetEntry(Expression<Func<Entry, bool>> expression);
+        public Task<EntriesResponseModel> GetEntries();
     }
 }
