c2454de1e0b8ae677660f566ed7e098ec992a172
Toyyib Omolola <fullstackbeast@gmail.com>
2022-04-20 04:15:54 -0500

    Added Migration

--- api/Startup.cs
+++ api/Startup.cs
@@ -6,10 +6,12 @@ using api.Implementation.Repositories;
 using api.Implementation.Services;
 using api.Interface.IRepositories;
 using api.Interface.IServices;
+using Api.Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,9 @@ namespace api
         {
 
             services.AddControllers();
+
+            services.AddDbContext<ApplicationContext>(option => option.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddScoped<IGameRepository, GameRepository>();
             services.AddScoped<IGameService, GameService>();
             services.AddSwaggerGen(c =>
